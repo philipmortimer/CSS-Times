6e3230b4185600220360b8b45ef88f755b025505
Philip Mortimer <philipmortimer99@aol.com>
2023-02-18 17:10:53 +0000

    Thumbail link + hover

--- css-times/src/Pages/Home/Home.js
+++ css-times/src/Pages/Home/Home.js
@@ -8,10 +8,6 @@ import React from 'react';
 export const Home = (props) => {
     return (
         <div className="homepage">
-
-    
-     
-            
             <div className="articles-container">
                 {getThumbnails(articles)}
             </div>
--- css-times/src/Pages/Home/Thumbnail.js
+++ css-times/src/Pages/Home/Thumbnail.js
@@ -1,28 +1,21 @@
 import './Thumbnail.css';
 import Moment from 'react-moment';
 
-function prettyDate(date){
-    var str = `${date.getDay()}/${date.getMonth()}/${date.getYear()}`
-    console.log(date)
-    return str;
-}
-
 export const Thumbnail = (props) => {
-    console.log(props.article)
     const myArticle = props.article
     return(
         <div className="thumbnail">
-            <img className="thumbnail-image" src={myArticle.mainPhoto.filePath} alt="really funny image" />
-            <div className="thumbnail-text">
-                <p className="thumbnail-headline"><strong>{myArticle.headline}</strong></p>
-                <p className="thumbnail-descr">{myArticle.summaryForHomePage}</p>
-                <Moment className="thumbnail-date" locale='uk' format='LL'
-                filter={(str) => str.replace(',', ' ')}>
-                    {props.article.publishDate}
-            </Moment>
-            </div>
-
+            <a className="articleLink" href={"/articles/" + props.article.id}>
+                <img className="thumbnail-image" src={myArticle.mainPhoto.filePath} alt="really funny image" />
+                <div className="thumbnail-text">
+                    <p className="thumbnail-headline"><strong>{myArticle.headline}</strong></p>
+                    <p className="thumbnail-descr">{myArticle.summaryForHomePage}</p>
+                    <Moment className="thumbnail-date" locale='uk' format='LL'
+                    filter={(str) => str.replace(',', ' ')}>
+                        {props.article.publishDate}
+                </Moment>
+                </div>
+            </a>
         </div>
-
     )
 }
