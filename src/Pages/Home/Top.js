0e2f74f1fff32bcb546f317e989a299f43977477
Philip Mortimer <philipmortimer99@aol.com>
2023-02-18 21:13:56 +0000

    Ensured top image loads

--- src/Pages/Home/Top.js
+++ src/Pages/Home/Top.js
@@ -1,11 +1,14 @@
 import './Top.css';
 import logo from './cssTimesLogoBlack.png'
+import {useState} from 'react';
 
 
 export const Top = (props) => {
+    const [_, setImageLoaded] = useState(false);
     return (
         <header>
-        <a href="/"><img className="Css-times-logo" src={logo} alt="" /></a>
+        <a href="/"><img className="Css-times-logo" onLoad={() => setImageLoaded(true)}
+        src={logo} alt="" /></a>
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/">Sport</a></li>
