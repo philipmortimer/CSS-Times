765bebcb9a52dbf3f53c6e116984941b2cabde32
liam-jones-2002 <um21605@bristol.ac.uk>
2023-02-18 16:53:51 +0000

    adding ruined marriage story

--- css-times/src/Articles/ArticleList.js
+++ css-times/src/Articles/ArticleList.js
@@ -1,10 +1,12 @@
 import { davidArticle } from "./DavidBernhard4Fa/DavidBernhard4Fa";
 import { davidArticleFINAL } from "./DB4Fact/DB4Fact";
 import { scotlandYard } from "./scotlandYardLawsuit/scotlandYardLawsuit";
+import { chatGPTRuinedMarriage } from "./chatGPTRuinedMarriage/chatGPTRuinedMarriage";
+
 
 
 // Assigns each article an ID
-const articlesWithouId = [davidArticle, davidArticleFINAL, scotlandYard];
+const articlesWithouId = [davidArticle, davidArticleFINAL, scotlandYard, chatGPTRuinedMarriage];
 for (let i = 0; i < articlesWithouId.length; i++) {
     articlesWithouId[i].setId(i);
 }
