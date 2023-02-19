aad5219ee09bf0c5205086b74599bc3d6feabcb7
Philip Mortimer <philipmortimer99@aol.com>
2023-02-19 09:43:38 +0000

    Please no errors

--- src/Ads/VisibleAd.js
+++ src/Ads/VisibleAd.js
@@ -3,22 +3,14 @@ import './VisibleAd.css'
 
 export const VisibleAd = (props) => {
     const myAd = props.ad;
-    if (myAd.linkHref.length === 0) return(
-        <div className="visibleAdWrapper">
-            <img className="ad-image"
-                 src={myAd.mainPhoto.filePath}
-                 alt={myAd.mainPhoto.caption} />
-        </div>
-    )
-
-    else return(
-        <div className="visibleAdWrapper">
-            <img className="ad-image"
-                 onLoad={() => {setImageLoaded(true);}}
-                 src={myAd.mainPhoto.filePath}
-                 alt={myAd.mainPhoto.caption} />
-                <button><a href={myAd.linkHref}>{myAd.linkText}</a></button>
-        </div>
+    const btn = myAd.linkHref.length === 0? null : <button><a href={myAd.linkHref}>{myAd.linkText}</a></button>;
+    return(
+    <div className="visibleAdWrapper">
+        <img className="ad-image"
+                src={myAd.mainPhoto.filePath}
+                alt={myAd.mainPhoto.caption} />
+        {btn}
+    </div>
     )
 }   
 
